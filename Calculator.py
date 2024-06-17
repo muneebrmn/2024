df5c1573858c815febd1dc471dee8786c7b64506
Muneeb Ur Rehman <muneebrmn19@gmail.com>
2024-06-18 01:36:51 +0500

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -1,18 +1,18 @@
 # Coded By Muneeb Ur Rehman
 # A Simple Calculator Capable Of Addition, Subtraction, Multiplication & Division Between Two Numbers.
 
-Num1 = int(input("Enter a number: "))
-Num2 = int(input("Enter a number: "))
+num_1 = int(input("Enter a number: "))
+num_2 = int(input("Enter a number: "))
 Operator = input("Enter an operator: ")
 
 if Operator == "+":
-  print(Num1 + Num2)
+  print(num_1 + num_2)
 elif Operator == "-":
-  print(Num1 - Num2)
+  print(num_1 - num_2)
 elif Operator == "*":
-  print(Num1 * Num2)
+  print(num_1 * num_2)
 elif Operator == "/":
-  print(Num1 / Num2)
+  print(num_1 / num_2)
 else:
   print("Please Input A Valid Operator And Try Again.")
 
